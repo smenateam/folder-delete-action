3b2a774f776444e8c41af1e1da6a7288b14c65e3
Inokenty <is@smena.space>
2020-01-31 12:58:46 +0500

    final fix

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const github = require('@actions/github')
 async function run() {
   try {
     const title = github.context.payload.pull_request.title
-    core.debug("ochko ilyi na barabane")
+    console.log("ochko ilyi na barabane")
     if (title) {
       const regex = new RegExp(/^([A-Z]+-\d+)/)
       const taskName = title.match(regex)[1]
-      core.debug('vashe ochko na barabane', taskName)
+      console.log('vashe ochko na barabane', taskName)
       core.setOutput('taskname', taskName)
     }
   } catch (error) {
